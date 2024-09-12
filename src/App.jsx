8ab357a80ee250c2279e909aaf7b1ea10def644a
malik1243 <muhammadali.84123@gmail.com>
2024-09-12 15:53:54 +0500

    website

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>React Router DOM</div>
+  )
+}
+
+export default App
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../Components/Card'
+
+const Contact = () => {
+  return (
+    <>
+      <Card title = "custom card"/>
+    </>
+  )
+}
+
+export default Contact
