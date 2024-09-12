88843bc04ee9a17cb8fab08ac9a33558f7f43471
malik1243 <muhammadali.84123@gmail.com>
2024-09-12 16:22:07 +0500

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav
-      className="navbar navbar-expand-lg navbar-dark bg-dark"
+      className="navbar navbar-expand-lg navbar-dark"
       style={{
         padding: "10px",
         justifyContent: "center", // added this to center the links
